638c22eff61be877bb6681b7d0f1798b71bf62ac
Jose Luiz da Silva Borges <A01196135@ad.bcit.ca>
2020-01-08 18:08:31 -0800

    Assigment 01

--- lesson01/jose-borges-lab1.js
+++ lesson01/jose-borges-lab1.js
@@ -1,22 +1,31 @@
+
+// Part 1
 window.onload = function () {
-    // Part 2
-    let firstname = prompt('Please type your firstname...');
-    let lastname = prompt('Please type your lastname...');
+// Part 2
+    let firstname = prompt('Please type your First Name...');
+    let lastname = prompt('Please type your Last Name...');
 
-    console.warn(`Welcome ${firstname} ${lastname},`)
+    console.log(`Welcome ${firstname} ${lastname}.`)
 
-    // Part 3
+// Part 3
     // clientWidth, offsetWidth 
     /*
       clientWidth: (inner width) Is the space inside a element including padding and excluding borders and scrollbars 
       offsetWidth: (outer width) Is the space occupied by the element including padding and borders space
 
-    */
 
-    // Part 4
-    let originalWithImg1 = document.getElementById('img1').with;
-    let img2 = document.getElementById('img2').style.width = originalWithImg1;
-    let img3 = document.getElementById('img3').style.width = originalWithImg1;
+      ------------------------------
+      The data types that width and style.width accept are:
+      width: Number 
+      style.width: String
+
+    */
+   
+
+// Part 4
+    let originalWithImg1 = document.getElementById('img1').clientWidth;
+    let img2 = document.getElementById('img2').width = originalWithImg1;
+    let img3 = document.getElementById('img3').width = originalWithImg1;
 
     
 }
