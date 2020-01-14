14a2afe782443d768f3a1b968f5d062dddc8c007
Jose Luiz da Silva Borges <A01196135@ad.bcit.ca>
2020-01-13 21:13:06 -0800

    Lesson02

---
+++ lesson02/site.js
@@ -0,0 +1,58 @@
+var $ = document;
+
+function ChangeColorOfElement(element, color) {
+    element.style.color = color;
+}
+ 
+let x = ["BCIT",1979,true];
+
+
+x[13] = "Jose"
+
+// x.push("Luiz")
+
+ 
+
+ let n = [1,2,3,5,5,6,7,8,9]
+
+ let ind = n.indexOf(5)
+
+ 
+
+//  let aofP = $.querySelectorAll('p')
+
+ 
+
+//   aofP.forEach(a => {
+//     ChangeColorOfElement(a,'blue')
+//  })
+
+//  // for (let index = 0; index < aofP.length; index++) {
+//     for (let index of aofP) {
+//       ChangeColorOfElement(index,'blue')
+     
+//   }
+
+
+let allLinks = document.links;
+
+for(let link of allLinks)
+{
+    link.href = `https://www.google.com/search?q=${link.innerText}`
+    link.target = '_blank';
+}
+  
+
+
+let num = 10
+if (num === "10")
+    console.log(` Won't printed`)
+
+// shortcut
+
+let isTusday = false
+
+if (!isTusday)
+{
+    console.log('Here?')
+}
