261871bf284b6c221c7f476910c1c8aea6c581d9
Jose <jose.iscosistemas@gmail.com  name = shpsyte [credential]  helper = cache --timeout 7200 [credential https://github.com/shpsyte]  username = shpsyte>
2020-01-17 16:18:38 -0800

    assigment02

--- lesson02/jose-borges-lab2.js
+++ lesson02/jose-borges-lab2.js
@@ -4,33 +4,54 @@
 
 let now = new Date();
 let hr = now.getHours();
-let mn =now.getMinutes();
-let msg = `${hr}:${mn}`
-alert(msg)
-
+let mn = now.getMinutes();
+let msg = `${hr}:${mn}`;
+//alert(msg);
 
 //Part 02
 //Create an array and fill it with at least six usernames (i.e. “Sophia”, “Gabriel”, …) then loop
 //through them with a for loop. If a username contains the letter “i” then alert the username.
 
-let usernames = ["Smith","Eva","Albert","Alexi","Mint","Ubuntu"]
+let usernames = ["Smith", "Eva", "Albert", "Alexi", "Mint", "Ubuntu"];
 
-for (let username of usernames)
-{
-    if (username.toLocaleLowerCase().includes("i"))
-    {
-        alert(` The username ${username} contains i...`)
-    }
-
+for (let username of usernames) {
+  if (username.toLocaleLowerCase().includes("i")) {
+    // alert(` The username ${username} contains [i]...`);
+  }
 }
 
 //Part 3
 //Use DOM 0 ( e.g. document.images ) to change the CSS style of the links so that every link
 //has its default underline removed (use the CSS text-decoration property for this).
 
-let allLinks = document.links;
+for (let link of document.querySelectorAll("a")) {
+  // link.classList.add("remove-text-decoration");
+  link.style.textDecoration = "none";
+}
 
-for(link of allLinks)
-{
-    link.className = "boxhead";
+//Part 4
+// Use DOM 1(e.g.document.getElementsByTagName(‘img’)) to:
+// – Add class ‘message’ to every div on the page.
+// – Change the width of every div to 100px
+// – Change the height of every div to 100px
+//– OPTIONAL - inside the for loop, use the code below to generate three
+//random values
+//then, combine the variables into a string literal(as shown below) and set the CSS
+//background color of a div to this value.Each of the six divs should then have //its own
+//distinct color.
+//`rgb(${r}, ${g}, ${b})`
+
+let divs = document.querySelectorAll("div");
+
+for (const div of divs) {
+  var r = Math.floor(Math.random() * 255);
+  var g = Math.floor(Math.random() * 255);
+  var b = Math.floor(Math.random() * 255);
+  let color = `rgb(${r}, ${g}, ${b})`;
+
+  div.classList.add("message");
+  div.style.width = "100px";
+  div.style.height = "100px";
+
+  div.style.backgroundColor = color;
 }
