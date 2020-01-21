fefd379d507d3a5c851985c534f0f58a3ea6f9c7
Jose Luiz da Silva Borges <A01196135@ad.bcit.ca>
2020-01-20 18:29:17 -0800

    task02

--- lesson02/jose-borges-lab2.js
+++ lesson02/jose-borges-lab2.js
@@ -6,7 +6,7 @@ let now = new Date();
 let hr = now.getHours();
 let mn = now.getMinutes();
 let msg = `${hr}:${mn}`;
-//alert(msg);
+alert(msg);
 
 //Part 02
 //Create an array and fill it with at least six usernames (i.e. “Sophia”, “Gabriel”, …) then loop
@@ -16,16 +16,16 @@ let usernames = ["Smith", "Eva", "Albert", "Alexi", "Mint", "Ubuntu"];
 
 for (let username of usernames) {
   if (username.toLocaleLowerCase().includes("i")) {
-    // alert(` The username ${username} contains [i]...`);
+     alert(` The username ${username} contains [i]...`);
   }
 }
 
 //Part 3
 //Use DOM 0 ( e.g. document.images ) to change the CSS style of the links so that every link
 //has its default underline removed (use the CSS text-decoration property for this).
+var links = document.links
 
-for (let link of document.querySelectorAll("a")) {
-  // link.classList.add("remove-text-decoration");
+for (let link of links ) {
   link.style.textDecoration = "none";
 }
 
